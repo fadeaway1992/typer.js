cac80d5435ca2df9f28f7b02a9a329d22d0530a2
fadeaway <498497303@qq.com>
2018-11-05 20:43:32 +0800

    init project

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+const presets = [
+  [
+    "@babel/env",
+    {
+      targets: "> 0.25%, not dead",
+      useBuiltIns: "usage",
+    },
+  ],
+];
+
+module.exports = { presets };
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack')
+const path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+  mode: 'development',
+  entry: [
+    './app.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: "dist.js"
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+      }
+    ]
+  },
+  devServer: {
+    contentBase: __dirname,
+    hot: true,
+    publicPath: '/',
+    compress: false,
+    port: 8000,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new HtmlWebpackPlugin()
+  ]
+}
